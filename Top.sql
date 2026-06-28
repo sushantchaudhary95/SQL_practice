5e64fe3c91136bf8dc15f282453be1be7c6141b6
sushantchaudhary95 <sushantchaudhary95@gmail.com>
2026-06-28 10:28:02 +0530

    Update Top.sql

--- Top.sql
+++ Top.sql
@@ -1,5 +1,9 @@
---Retrieve only 3 Customers with the heighest scores
+--Retrieve only 3 Customers with the heighest scores and lowest scores
 
 Select Top 3*
 From customers
 order by score Desc
+
+Select Top 3*
+From customers
+order by score Asc
